78c792c2253118ab41fb8c0c1eb39833154dfd56
diegorchagas <drochachagas@gmail.com>
2016-10-26 07:55:36 -0200

    Adição do swiper

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+var swiper = new Swiper('.swiper-container', {
+    pagination: '.swiper-pagination',
+    paginationClickable: true,
+    nextButton: '.swiper-button-next',
+    prevButton: '.swiper-button-prev',
+    spaceBetween: 30
+});
+    
