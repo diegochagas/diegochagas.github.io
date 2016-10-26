3b3b72cf27f24f40a696689345c27ee09bf72a24
diegorchagas <drochachagas@gmail.com>
2016-10-26 21:08:40 -0200

    Formatação do conteúdo

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
 var swiper = new Swiper('.swiper-container', {
     pagination: '.swiper-pagination',
     paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
     spaceBetween: 30
 });
     
