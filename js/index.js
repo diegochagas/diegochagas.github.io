0727dbab90f7cb506f658595fc3ea2fad559a353
diegorchagas <drochachagas@gmail.com>
2016-11-23 22:06:43 -0200

    rotação engrenagens

--- js/index.js
+++ js/index.js
@@ -2,3 +2,10 @@ $("#more-works").click(function(){
 	$("button span.glyphicon").toggleClass("glyphicon-menu-up");
     $("button span.glyphicon").toggleClass("glyphicon-menu-down");
 });
+
+setTimeout(function(){
+	$(".gear").css("animation", "gear-rotate 10s infinite");
+	$(".gear-small .bar-small:nth-child(2)").css("animation", "bar-rotate1 10s infinite");
+	$(".gear-small .bar-small:nth-child(3)").css("animation", "bar-rotate2 10s infinite");
+	$(".gear-small .bar-small:last-child").css("animation", "bar-rotate3 10s infinite");
+}, 2000);
