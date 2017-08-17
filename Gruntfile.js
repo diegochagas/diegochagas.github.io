1f9b34cc6fe851cbde7e8b329c2e7efecabbd75e
Diego R Chagas <diego@keyrus.com.br>
2017-08-17 10:59:16 -0300

    minication

--- Gruntfile.js
+++ Gruntfile.js
@@ -39,13 +39,24 @@ module.exports = function(grunt){
 					baseDir: "./"
 				}
 			}
+		},
+		useminPrepare: {
+			html: "dist/**/*.html"
+		},
+		usemin: {
+			html: "dist/**/*.html"
 		}
 
 	});
 
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-cssmin");
+	grunt.loadNpmTasks("grunt-usemin");
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks("grunt-contrib-watch");
 	grunt.loadNpmTasks("grunt-browser-sync");
 
-	grunt.registerTask("default", ["browserSync","watch"]);
+	grunt.registerTask("minifica", ["useminPrepare", "concat", "uglify", "cssmin", "usemin"]);
+	grunt.registerTask("default", ["browserSync", "watch"]);
 }
