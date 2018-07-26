69a3c444214bf76725cfef6ff82756b6395b8f09
diegorchagas <drochachagas@gmail.com>
2018-07-27 07:34:58 +1200

    html content in only one file

--- js/index.js
+++ js/index.js
@@ -1,42 +1,21 @@
-
-// Alterna as direções do ícone de seta entre baixo e cima.
-$("#more-works, #records-fals, #records-etec").click(function(){
-	$("span.glyphicon", this).toggleClass("glyphicon-menu-up");
-    $("span.glyphicon", this).toggleClass("glyphicon-menu-down");
-    var moreWorks = $(this).is("#more-works");
-    if(moreWorks && $(this).attr("aria-expanded") == "true")
-    	$(".text", this).text("More works");
-    if(moreWorks && $(this).attr("aria-expanded") == "false")
-    	$(".text", this).text("Less works");
-
-});
-
-// Load the HTML
 $(document).ready(function(){
-    loadHome();
+    // Alterna as direções do ícone de seta entre baixo e cima.
+    $("#more-works, #records-fals, #records-etec").click(function(){
+        $("span.glyphicon", this).toggleClass("glyphicon-menu-up");
+        $("span.glyphicon", this).toggleClass("glyphicon-menu-down");
+        var moreWorks = $(this).is("#more-works");
+        if(moreWorks && $(this).attr("aria-expanded") == "true")
+            $(".text", this).text("More works");
+        if(moreWorks && $(this).attr("aria-expanded") == "false")
+            $(".text", this).text("Less works");
+    });
+
+    // Carrega a animação de rotação da engrenagem após 2 segundos
+    setTimeout(function(){
+    	$(".gear").css("animation", "gear-rotate 10s infinite");
+    	$(".gear-small .bar-small:nth-child(2)").css("animation", "bar-rotate1 10s infinite");
+    	$(".gear-small .bar-small:nth-child(3)").css("animation", "bar-rotate2 10s infinite");
+    	$(".gear-small .bar-small:last-child").css("animation", "bar-rotate3 10s infinite");
+    }, 2000);
 });
 
-// Carrega a animação de rotação da engrenagem após 2 segundos
-setTimeout(function(){
-	$(".gear").css("animation", "gear-rotate 10s infinite");
-	$(".gear-small .bar-small:nth-child(2)").css("animation", "bar-rotate1 10s infinite");
-	$(".gear-small .bar-small:nth-child(3)").css("animation", "bar-rotate2 10s infinite");
-	$(".gear-small .bar-small:last-child").css("animation", "bar-rotate3 10s infinite");
-}, 2000);
-
-function loadHome(){
-    $("#content-page").load("views/home.html");   
-}
-
-function loadFals(){
-    $("#content-page").load("views/fals.html");
-}
-
-function loadEtec(){
-    $("#content-page").load("views/etec.html");
-}
-
-function goToHeader(){
-    var headerTop =  $("header").offset().top;
-    $(window).scrollTop(headerTop);
-}
