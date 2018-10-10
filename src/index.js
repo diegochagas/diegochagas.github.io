269586ee0755035af6d690536b9b01a996ddda98
diegochagas <drochachagas@gmail.com>
2018-10-10 16:10:11 -0300

    Remove certificate from this project

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
-import certificateAbacus from './components/certificate_abacus.js';
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
-	<BrowserRouter>
-		<Switch>
-			<Route path="/certificates/abacus-english" component={certificateAbacus} />
-			<Route path="/" component={App} />
-		</Switch>
-	</BrowserRouter>
-	, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
