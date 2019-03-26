b953f8f91838823aa4ba8bf772be2d7dc44ed466
Diego Rocha Chagas <drochachagas@gmail.com>
2019-03-26 13:14:12 -0300

    Update section in the article About Me

--- src/components/about.js
+++ src/components/about.js
@@ -19,13 +19,14 @@ export default class About extends Component{
 					<div className="container">
 						<h3>ABOUT ME</h3>
 						<div className="strip text-center"><img src={iconAbout} alt="icon about"/></div>
-						<p>Started as a technology professional in December 2011, but began the studies in developer technology in 2008, where worked on many projects, focused on web development. Advanced knowledge in front-end and intermediate knowledge in back-end. Before that, worked with public administration and as a freelancer in video editing and motion design. Major works using Node and Java platforms.</p>
+						<p>Started as a developer professional in March 2016, but began the studies in developer technology in 2008, where worked on many projects, focused on web development. Advanced knowledge in front-end and intermediate knowledge in back-end. Before that, worked with public administration and as a freelancer in video editing and motion design. Major works using Node and Java platforms.</p>
 						<p>Other highlights of my career would be:</p>
 						<ul className="highlights-list">
-							<li>Constructed mobile web pages, extracting the content of desktop sites, with frameworks based in JavaScript, like Node.js and Handlebars.js.</li>
-							<li>Utilize extensive software development skills to develop components in JavaScript.</li>
-							<li>Created landing pages with layouts construct in HTML and CSS and JSTL to the GrandOptical, optical and service of group GrandVision in France.</li>
-							<li>Utilize Scrum methodology.</li>
+							<li>Utilize extensive software development skills to develop components in React and Redux.</li>
+							<li>Back-end development with Node.js and MongoDB.</li>
+							<li>Constructed web page components with Angular.</li>
+							<li>Created landing pages with layouts constructed in HTML, CSS, JavaScript and SASS/LESS.</li>
+							<li>Utilize agile methodology.</li>
 						</ul>
 						<ul>
 							{this.renderSocialLinks("fb", "https://www.facebook.com/diegorochachagas")}
