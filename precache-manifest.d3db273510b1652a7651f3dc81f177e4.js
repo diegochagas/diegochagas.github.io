113910881c50e107b93d2779dde4fcecc915241f
Diego Rocha Chagas <drochachagas@gmail.com>
2019-03-26 12:56:16 -0300

    Updates

---
+++ precache-manifest.d3db273510b1652a7651f3dc81f177e4.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5b9f962c14d9b4b6d32163780f423875",
+    "url": "/static/media/banner6.5b9f962c.jpg"
+  },
+  {
+    "revision": "50a433d5425b18d919bd",
+    "url": "/static/css/main.b98b675a.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d932cb475a954639ca6f",
+    "url": "/static/js/2.142bc425.chunk.js"
+  },
+  {
+    "revision": "0fbe886b7e5a3b22d278bdf149c25a5d",
+    "url": "/static/media/banner1.0fbe886b.jpg"
+  },
+  {
+    "revision": "d7cadc588ff0c9a326309d51c1bac8c6",
+    "url": "/static/media/banner2.d7cadc58.jpg"
+  },
+  {
+    "revision": "d7cbeabb518c786a6c719c75de7a4656",
+    "url": "/static/media/banner3.d7cbeabb.jpg"
+  },
+  {
+    "revision": "50a433d5425b18d919bd",
+    "url": "/static/js/main.8fef432f.chunk.js"
+  },
+  {
+    "revision": "9e0736516bcc2432abcfa960221b49e9",
+    "url": "/static/media/banner7.9e073651.jpg"
+  },
+  {
+    "revision": "52defb50022f8edc9616221b4f29d480",
+    "url": "/static/media/banner8.52defb50.jpg"
+  },
+  {
+    "revision": "8ec3199c5e4e23d84e6b675d01ccc6a8",
+    "url": "/static/media/banner.8ec3199c.jpg"
+  },
+  {
+    "revision": "e8b61dfc9a0a462af927f08199abe891",
+    "url": "/static/media/icons.e8b61dfc.png"
+  },
+  {
+    "revision": "29b1280e2ff3053f71e3c04cb1e1ffb8",
+    "url": "/static/media/port-back.29b1280e.jpg"
+  },
+  {
+    "revision": "d60492883da3a94e5013f59107be257f",
+    "url": "/index.html"
+  }
+];
