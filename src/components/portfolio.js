687df39e047b5896d9966664c274a54d24533a3d
Diego Rocha Chagas <drochachagas@gmail.com>
2019-03-28 10:17:36 -0300

    Create an array  map to show the portfolios

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -3,38 +3,39 @@ import iconPort from "../images/port.png";
 import portfolio1 from "../images/banner1.jpg";
 import portfolio2 from "../images/banner2.jpg";
 import portfolio3 from "../images/banner3.jpg";
+import portfolio4 from "../images/banner4.jpg";
+import portfolio5 from "../images/banner5.jpg";
 import portfolio6 from "../images/banner6.jpg";
-import portfolio7 from "../images/banner7.jpg";
-import portfolio8 from "../images/banner8.jpg";
 
 export default class Portfolio extends Component{
-	renderWork(portfolio, company, role, site){
-		return(
-			<figure className="col-md-4 gallery-grid gallery1">
-				<img src={portfolio} className="img-responsive" alt="print of the page"/>
-				<figcaption className="textbox">
-					<h4>{company}</h4>
-					<p>{role}</p>
-					<div className="button"><a href={site} target="_blank" rel="noopener noreferrer">VIEW</a></div>
-				</figcaption>
-			</figure>
-		);
-	}
-
 	render(){
-		return(
+    const portfolios = [
+      {imgSrc: portfolio2, company: "Santander", role: "SOFTWARE ENGINEER", url: "https://www.santander.com.br/"},
+      {imgSrc: portfolio5, company: "Carrefour", role: "FRONT-END", url: "https://www.carrefour.com.br/"},
+      {imgSrc: portfolio4, company: "Grand Optical", role: "FRONT-END", url: "https://www.grandoptical.com/fr/"},
+      {imgSrc: portfolio1, company: "Underarmour", role: "FRONT-END", url: "https://www.underarmour.com.br/pt-br/"},
+      {imgSrc: portfolio3, company: "Nestlé", role: "FRONT-END", url: "https://www.nestle.com.br/"},
+      {imgSrc: portfolio6, company: "Pacifika", role: "FRONT-END", url: "https://www.pacifika.com/"}
+		];
+
+	  return(
 			<section className="gallery-section text-center" id="portfolio">
 				<div className="container">
 					<h3>PORTFOLIO</h3>
 					<div className="strip text-center"><img src={iconPort} alt="icon portfolio"/></div>
 					<div className="gallery-grids">
 						<div className="top-gallery">
-							{this.renderWork(portfolio8, "Carrefour", "FRONT-END", "https://www.carrefour.com.br/")}
-							{this.renderWork(portfolio7, "Grand Optical", "FRONT-END", "https://www.grandoptical.com/fr/")}
-							{this.renderWork(portfolio1, "Underarmour", "MOBILE DEVELOPING", "https://www.underarmour.com.br/pt-br/")}
-							{this.renderWork(portfolio2, "Giuliana Flores", "MOBILE DEVELOPING", "http://m2.giulianaflores.com.br/")}
-							{this.renderWork(portfolio3, "Nestlé", "MOBILE DEVELOPING", "https://www.nestle.com.br/")}
-							{this.renderWork(portfolio6, "Pacifika", "FRONT-END", "https://www.pacifika.com/")}
+              {portfolios.map(portfolio => {
+                return(
+                  <figure className="col-md-4 gallery-grid gallery1" key={portfolio.company}>
+                    <img src={portfolio.imgSrc} className="img-responsive" alt="print of the page"/>
+                    <figcaption className="textbox">
+                      <h4>{portfolio.company}</h4>
+                      <p>{portfolio.role}</p>
+                      <div className="button"><a href={portfolio.url} target="_blank" rel="noopener noreferrer">VIEW</a></div>
+                    </figcaption>
+                  </figure>
+                )})}
 							<div className="clearfix"> </div>
 						</div>
 					</div>
